6cac92f3e776272f30c1faa9c9688d9e2f9b2c9e
navin <navinvannam1999@gmail.com>
2023-04-18 17:24:33 +0530

    commit user login project
    Signed-off-by: navin <navinvannam1999@gmail.com>
    
    Change-Id: I566206eb17f346fc7ffbf8f114f08ef967223bb3

---
+++ UserLoginPage/src/main/java/LogoutServlet.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LogoutServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        HttpSession session=request.getSession();  
+        session.invalidate();           
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("Logged out of session.<br>");
+
+		
+        out.println("</body></html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
